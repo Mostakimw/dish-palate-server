59b7b59a1da37cd19717859cf0847c1dd12c07c7
Mostakimw <scientist.expert@gmail.com>
2024-05-24 09:35:34 +0600

    all recipe data api complete with filtering and searching data

--- index.js
+++ index.js
@@ -25,15 +25,16 @@ async function run() {
     console.log("Connected to MongoDB");
 
     const db = client.db("dish-palate");
-    const collection = db.collection("users");
+    const usersCollection = db.collection("users");
+    const recipesCollection = db.collection("recipes");
 
-    // User Registration
+    //! User Registration
     app.post("/api/v1/register", async (req, res) => {
       const { displayName, photoUrl, email, coin } = req.body;
       console.log(req.body);
 
       // Check if email already exists
-      const existingUser = await collection.findOne({ email });
+      const existingUser = await usersCollection.findOne({ email });
       if (existingUser) {
         return res.status(400).json({
           success: false,
@@ -48,6 +49,55 @@ async function run() {
         success: true,
         message: "User registered successfully",
       });
+    });
+
+    //! recipe create api
+    app.post("/api/v1/recipe", async (req, res) => {
+      const data = req.body;
+      try {
+        const result = await recipesCollection.insertOne(data);
+        res.status(201).json({
+          success: true,
+          message: "Recipe created successfully",
+          data: data,
+        });
+      } catch (error) {
+        res.status(500).json({
+          success: false,
+          message: "An error occurred while creating recipe data",
+        });
+      }
+    });
+
+    //! recipe get and filtering and search
+    app.get("/api/v1/recipes", async (req, res) => {
+      const { category, country, search } = req.query;
+      let query = {};
+
+      if (category) {
+        query.category = category;
+      }
+      if (country) {
+        query.country = country;
+      }
+
+      if (search) {
+        query.recipeName = { $regex: search, $options: "i" };
+      }
+
+      try {
+        const recipes = await recipesCollection.find(query).toArray();
+        res.status(200).json({
+          success: true,
+          message: "Recipes fetched successfully",
+          data: recipes,
+        });
+      } catch (error) {
+        res.status(500).json({
+          success: false,
+          message: "An error occurred while fetching recipes",
+        });
+      }
     });
 
     // Start the server
